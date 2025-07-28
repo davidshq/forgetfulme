c76512a5c8f3641f9fb6eecc793856e0c82415a3
Dave Mackey <dave@davemackey.net>
2025-07-28 13:16:38 -0400

    Resetting to scratch

--- bookmark-management/state/supabase.js
+++
@@ -1,5 +0,0 @@
-// Supabase data logic for bookmark management
-
-// TODO: Move SupabaseService-related logic here if needed
-
-export {};
--- popup/state/auth.js
+++
@@ -1,3 +0,0 @@
-// Auth state management for popup
-export async function initializeAuthState(_ctx) {}
-export function handleAuthStateChange(_ctx, _session) {}
